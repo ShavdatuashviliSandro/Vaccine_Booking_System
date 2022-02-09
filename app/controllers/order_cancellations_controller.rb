2ddb320a2662a28d6be2c4fea8b98378ffab5c75
Sandro Shavdatuashvili <sandro.shav@gmail.com>
2022-02-09 21:29:11 +0400

    Refactoring methods

--- app/controllers/order_cancellations_controller.rb
+++ app/controllers/order_cancellations_controller.rb
@@ -1,50 +1,47 @@
 class OrderCancellationsController < ApplicationController
-  def index; end
-
   def fetch_order_code
     @order_code_value = params[:order_code]
     session[:passed_variable] = @order_code_value
 
-    @orders=Order.find_by(order_code: params[:order_code])
+    @orders = Order.find_by(order_code: params[:order_code])
+    @inputs_correct = @orders&.patient&.mobile_phone == params[:mobile_phone]
 
-    if @orders&.patient&.mobile_phone == params[:mobile_phone]
-      render partial: 'form'
-    else
-      render partial: 'error'
-    end
+    return render partial: 'form' if @inputs_correct
+
+    render partial: 'error'
   end
 
   def change_status
-    @verify_sms_value=params[:search_sms]
+    @verify_sms_value = params[:search_sms]
     @order_code_value = session[:passed_variable]
 
-    @orders=Order.find_by(order_code: @order_code_value)
+    @orders = Order.find_by(order_code: @order_code_value)
 
-    @booking=Booking.find_by(order_id: @orders.id)
-    @verify_sms=VerifySmsMessage.where(booking_id: @booking.id).last.code
+    @booking = Booking.find_by(order_id: @orders.id)
+    @verify_sms = VerifySmsMessage.where(booking_id: @booking.id).last.code
 
-    if @verify_sms_value==@verify_sms
-      return order_cancel if @orders.finished?
-      order_reactive
-    end
+    return unless @verify_sms_value == @verify_sms
+    return order_cancel if @orders.finished?
+
+    order_reactive
   end
 
   def call
-    @booking=Booking.find_by(order_id: params[:order_id])
-    sms=Web::GenerateVerifySmsService.new(@booking).call
+    @booking = Booking.find_by(order_id: params[:order_id])
+    sms = Web::GenerateVerifySmsService.new(@booking).call
     SendVerifySmsWorker.perform_async(sms.id)
   end
 
   private
 
   def order_cancel
-    @orders.finished=false
+    @orders.finished = false
     @orders.save
     redirect_to root_url, notice: I18n.t('web.order_cancellation.list.order_cancel')
   end
 
   def order_reactive
-    @orders.finished=true
+    @orders.finished = true
     @orders.save
     redirect_to root_url, notice: I18n.t('web.order_cancellation.list.order_cancel')
   end
